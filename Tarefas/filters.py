ed94b3d5347057db6b27398786dd6dd6b594be26
Falcomxtreme01~ <you@example.comfelipesmking@gmail.com>
2024-07-09 21:40:24 -0300

    Fix: filters.py

--- Tarefas/filters.py
+++ Tarefas/filters.py
@@ -1,13 +1,12 @@
 import django_filters
-from .models import Tarefa
+
+from .models import  Tarefa
 
 #Filtro a ser aperfeiçoado!
-
-class FiltroDeTarefas(django_filters.FilterSet):         
+class FiltoDeStatus(django_filters.FilterSet):         
 
     class Meta:
         model = Tarefa
         fields = {
             'Status',
-            'DataDeVencimento'
         }
