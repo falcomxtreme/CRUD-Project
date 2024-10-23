99deeb43895a9ebe0658992426cd3ec62713229f
Falcomxtreme01~ <you@example.comfelipesmking@gmail.com>
2024-10-23 14:14:18 -0300

    Add: Documentation

--- Tarefas/views.py
+++ Tarefas/views.py
@@ -1,4 +1,3 @@
-from typing import Any
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
@@ -10,16 +9,19 @@ class ListaDeTarefasView(ListView):
     model = Tarefa
     nome_do_template = 'tarefa_list.html'
     context_object_name = 'tarefas'
-
+    
+    # Função para definir as tarefas exibidas.
     def get_queryset(self):
+        #Cria a QS com todas as tarefas.
         queryset = super().get_queryset()
+        #GET para pegar o valor de status, caso haja.
         status = self.request.GET.get('status')
-
+        #Se houver valor em status, filtra a QS para as que possuírem tal valor.
         if status:
             queryset = queryset.filter(status=status)
-            print(status)
         return queryset
-
+    
+    # Função para pegar os valores para o filtro de status
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Adiciona a lista de status ao contexto para exibição no template
